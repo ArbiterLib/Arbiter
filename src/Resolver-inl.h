352c2450854fbb56f164a439b2abd5c493068369
Justin Spahr-Summers <justin@jspahrsummers.com>
2016-08-18 23:06:50 +0100

    Implement dependency list fetch "behavior"

--- src/Resolver-inl.h
+++ src/Resolver-inl.h
@@ -8,21 +8,76 @@
 #include "Dependency-inl.h"
 #include "Resolver.h"
 #include "Value-inl.h"
+#include "Version-inl.h"
+#include "internal/Hash.h"
+
+#include <future>
+#include <mutex>
+#include <unordered_map>
+
+namespace Arbiter {
+namespace Resolver {
+
+struct ResolvedDependency
+{
+  ArbiterProjectIdentifier projectIdentifier;
+  ArbiterSelectedVersion selectedVersion;
+
+  static ResolvedDependency takeOwnership (ArbiterDependencyListFetch fetch) noexcept;
+
+  bool operator== (const ResolvedDependency &other) const noexcept;
+};
+
+}
+}
+
+namespace std {
+
+template<>
+struct hash<Arbiter::Resolver::ResolvedDependency>
+{
+  public:
+    size_t operator() (const Arbiter::Resolver::ResolvedDependency &fetch) const
+    {
+      return Arbiter::hashOf(fetch.projectIdentifier)
+        ^ Arbiter::hashOf(fetch.selectedVersion);
+    }
+};
+
+}
 
 struct ArbiterResolver
 {
   public:
-    ArbiterResolverBehaviors _behaviors;
-    ArbiterDependencyList _remainingDependencies;
     Arbiter::SharedUserValue _context;
 
     ArbiterResolver (ArbiterResolverBehaviors behaviors, ArbiterDependencyList dependencyList, Arbiter::SharedUserValue context)
-      : _behaviors(std::move(behaviors))
+      : _context(std::move(context))
+      , _behaviors(std::move(behaviors))
       , _remainingDependencies(std::move(dependencyList))
-      , _context(std::move(context))
     {}
 
+    std::future<ArbiterDependencyList> fetchDependencyList (Arbiter::Resolver::ResolvedDependency fetch);
+
+    bool resolvedAll () const noexcept;
     void resolveNext (ArbiterResolverCallbacks callbacks);
+
+  private:
+    const ArbiterResolverBehaviors _behaviors;
+    ArbiterDependencyList _remainingDependencies;
+
+    std::mutex _fetchesMutex;
+    std::unordered_map<Arbiter::Resolver::ResolvedDependency, std::promise<ArbiterDependencyList>> _dependencyListFetches;
+
+    std::future<ArbiterDependencyList> insertDependencyListFetch (Arbiter::Resolver::ResolvedDependency fetch);
+    std::promise<ArbiterDependencyList> extractDependencyListFetch (const Arbiter::Resolver::ResolvedDependency &fetch);
+
+    // C exports
+    static void dependencyListFetchOnSuccess (ArbiterDependencyListFetch cFetch, const ArbiterDependencyList *fetchedList);
+    static void dependencyListFetchOnError (ArbiterDependencyListFetch cFetch);
+    static void availableVersionsFetchOnNext (ArbiterAvailableVersionsFetch cFetch, const ArbiterSelectedVersion *nextVersion);
+    static void availableVersionsFetchOnCompleted (ArbiterAvailableVersionsFetch cFetch);
+    static void availableVersionsFetchOnError (ArbiterAvailableVersionsFetch cFetch);
 };
 
 #endif
--- src/Resolver.cpp
+++ src/Resolver.cpp
@@ -1,6 +1,10 @@
 #include "Resolver-inl.h"
 
+#include <exception>
+#include <stdexcept>
+
 using namespace Arbiter;
+using namespace Resolver;
 
 ArbiterResolver *ArbiterCreateResolver (ArbiterResolverBehaviors behaviors, const ArbiterDependencyList *dependencyList, ArbiterUserValue context)
 {
@@ -14,7 +18,7 @@ const void *ArbiterResolverContext (const ArbiterResolver *resolver)
 
 bool ArbiterResolvedAllDependencies (const ArbiterResolver *resolver)
 {
-  return resolver->_remainingDependencies._dependencies.empty();
+  return resolver->resolvedAll();
 }
 
 void ArbiterStartResolvingNextDependency (ArbiterResolver *resolver, ArbiterResolverCallbacks callbacks)
@@ -27,7 +31,80 @@ void ArbiterFreeResolver (ArbiterResolver *resolver)
   delete resolver;
 }
 
+ResolvedDependency ResolvedDependency::takeOwnership (ArbiterDependencyListFetch fetch) noexcept
+{
+  std::unique_ptr<ArbiterProjectIdentifier> project(const_cast<ArbiterProjectIdentifier *>(fetch.project));
+  std::unique_ptr<ArbiterSelectedVersion> selectedVersion(const_cast<ArbiterSelectedVersion *>(fetch.selectedVersion));
+
+  return ResolvedDependency { std::move(*project), std::move(*selectedVersion) };
+}
+
+bool ResolvedDependency::operator== (const ResolvedDependency &other) const noexcept
+{
+  return projectIdentifier == other.projectIdentifier && selectedVersion == other.selectedVersion;
+}
+
+bool ArbiterResolver::resolvedAll () const noexcept
+{
+  return _remainingDependencies._dependencies.empty();
+}
+
 void ArbiterResolver::resolveNext (ArbiterResolverCallbacks callbacks)
 {
   assert(false);
+}
+
+std::future<ArbiterDependencyList> ArbiterResolver::insertDependencyListFetch (ResolvedDependency dependency)
+{
+  std::lock_guard<std::mutex> guard(_fetchesMutex);
+
+  return _dependencyListFetches[std::move(dependency)].get_future();
+}
+
+std::promise<ArbiterDependencyList> ArbiterResolver::extractDependencyListFetch (const ResolvedDependency &dependency)
+{
+  std::lock_guard<std::mutex> guard(_fetchesMutex);
+
+  std::promise<ArbiterDependencyList> promise = std::move(_dependencyListFetches.at(dependency));
+  _dependencyListFetches.erase(dependency);
+
+  return promise;
+}
+
+std::future<ArbiterDependencyList> ArbiterResolver::fetchDependencyList (ResolvedDependency dependency)
+{
+  // Eventually freed in the C callback function.
+  auto project = std::make_unique<ArbiterProjectIdentifier>(dependency.projectIdentifier);
+  auto version = std::make_unique<ArbiterSelectedVersion>(dependency.selectedVersion);
+
+  auto future = insertDependencyListFetch(std::move(dependency));
+
+  _behaviors.fetchDependencyList(
+    ArbiterDependencyListFetch { this, project.release(), version.release() },
+    &dependencyListFetchOnSuccess,
+    &dependencyListFetchOnError
+  );
+
+  return future;
+}
+
+void ArbiterResolver::dependencyListFetchOnSuccess (ArbiterDependencyListFetch cFetch, const ArbiterDependencyList *fetchedList)
+{
+  auto &resolver = *const_cast<ArbiterResolver *>(cFetch.resolver);
+  auto dependency = ResolvedDependency::takeOwnership(cFetch);
+
+  resolver
+    .extractDependencyListFetch(dependency)
+    .set_value(*fetchedList);
+}
+
+void ArbiterResolver::dependencyListFetchOnError (ArbiterDependencyListFetch cFetch)
+{
+  auto &resolver = *const_cast<ArbiterResolver *>(cFetch.resolver);
+  auto dependency = ResolvedDependency::takeOwnership(cFetch);
+
+  resolver
+    .extractDependencyListFetch(dependency)
+    // TODO: Better error reporting
+    .set_exception(std::make_exception_ptr(std::runtime_error("Dependency list fetch failed")));
 }
