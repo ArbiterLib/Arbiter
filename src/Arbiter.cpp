60afe680b66cb9ab2bf63a0def05e7fc7b8872c9
Justin Spahr-Summers <justin@jspahrsummers.com>
2016-08-19 21:34:20 +0100

    Remove unused Arbiter.{cpp,h}

--- src/Arbiter.cpp
+++
@@ -1,7 +0,0 @@
-#include "Arbiter.h"
-
-#include <iostream>
-
-void say_hi() {
-  std::cout << "Hello world" << std::endl;
-}
--- src/Arbiter.h
+++
@@ -1,9 +0,0 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void say_hi();
-
-#ifdef __cplusplus
-}
-#endif
