81185edad259b2ef013f267b829502704908d4ea
Justin Spahr-Summers <justin@jspahrsummers.com>
2016-08-29 18:02:23 +0100

    Finish SemanticVersion bindings

--- bindings/swift/Arbiter/Version.swift
+++ bindings/swift/Arbiter/Version.swift
@@ -1,6 +1,6 @@
-public class SemanticVersion
+public class SemanticVersion : Equatable, Comparable
 {
-  init (major: Int, minor: Int, patch: Int, prereleaseVersion: String? = nil, buildMetadata: String? = nil) {
+  public init (major: Int, minor: Int, patch: Int, prereleaseVersion: String? = nil, buildMetadata: String? = nil) {
     // Makes the compiler happy
     _semanticVersion = nil
 
@@ -11,12 +11,61 @@ public class SemanticVersion
     }
   }
 
+  public init? (fromString str: String) {
+    let ptr = ArbiterCreateSemanticVersionFromString(str)
+    if ptr == nil {
+      return nil
+    }
+
+    _semanticVersion = UnsafeMutablePointer(ptr)
+  }
+
   deinit {
     _semanticVersion.destroy()
     _semanticVersion.dealloc(1)
   }
 
+  public var major: Int {
+    return Int(ArbiterGetMajorVersion(COpaquePointer(_semanticVersion)))
+  }
+
+  public var minor: Int {
+    return Int(ArbiterGetMinorVersion(COpaquePointer(_semanticVersion)))
+  }
+
+  public var patch: Int {
+    return Int(ArbiterGetPatchVersion(COpaquePointer(_semanticVersion)))
+  }
+
+  public var prereleaseVersion: String? {
+    let str = ArbiterGetPrereleaseVersion(COpaquePointer(_semanticVersion))
+    if str == nil {
+      return nil
+    }
+
+    return String.fromCString(str)
+  }
+
+  public var buildMetadata: String? {
+    let str = ArbiterGetBuildMetadata(COpaquePointer(_semanticVersion))
+    if str == nil {
+      return nil
+    }
+
+    return String.fromCString(str)
+  }
+
   private let _semanticVersion: UnsafeMutablePointer<Void>
+}
+
+public func == (lhs: SemanticVersion, rhs: SemanticVersion) -> Bool
+{
+  return ArbiterEqualVersions(COpaquePointer(lhs._semanticVersion), COpaquePointer(rhs._semanticVersion))
+}
+
+public func < (lhs: SemanticVersion, rhs: SemanticVersion) -> Bool
+{
+  return ArbiterCompareVersionOrdering(COpaquePointer(lhs._semanticVersion), COpaquePointer(rhs._semanticVersion)) < 0
 }
 
 private func maybeWithCString<Result> (str: String?, @noescape f: UnsafePointer<Int8> throws -> Result) rethrows -> Result
