20b9877c7aebf09516c1c24b070fcf9165658644
Justin Spahr-Summers <justin@jspahrsummers.com>
2016-08-29 23:18:10 +0100

    Implement Dependency

--- bindings/swift/Arbiter/Dependency.swift
+++ bindings/swift/Arbiter/Dependency.swift
@@ -16,3 +16,27 @@ public final class ProjectIdentifier<Value: AnyObject where Value: Comparable> :
     return fromUserValue(ArbiterProjectIdentifierValue(pointer))
   }
 }
+
+public final class Dependency<ProjectValue: AnyObject where ProjectValue: Comparable> : CObject
+{
+  public override init (_ pointer: COpaquePointer, shouldCopy: Bool = true)
+  {
+    super.init(pointer, shouldCopy: shouldCopy)
+  }
+
+  public convenience init (project: ProjectIdentifier<ProjectValue>, requirement: Requirement)
+  {
+    let ptr = ArbiterCreateDependency(project.pointer, requirement.pointer)
+    self.init(ptr, shouldCopy: false)
+  }
+
+  public var project: ProjectIdentifier<ProjectValue>
+  {
+    return ProjectIdentifier<ProjectValue>(ArbiterDependencyProject(pointer))
+  }
+
+  public var requirement: Requirement
+  {
+    return Requirement(ArbiterDependencyRequirement(pointer))
+  }
+}
