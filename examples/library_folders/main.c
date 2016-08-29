42c8931b05abe38c9be940aba2538c2648b7710b
Justin Spahr-Summers <justin@jspahrsummers.com>
2016-08-29 14:27:38 +0100

    Fix check of chdir() result

--- examples/library_folders/main.c
+++ examples/library_folders/main.c
@@ -35,7 +35,7 @@ int main (int argc, const char **argv)
   }
 
   const char *path = argv[1];
-  if (!chdir(path)) {
+  if (chdir(path) != 0) {
     perror("chdir()");
     return EXIT_FAILURE;
   }
