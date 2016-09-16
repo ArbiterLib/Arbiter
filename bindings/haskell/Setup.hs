8fb2526d77cb417801d27ae8fe232c34f1534b2c
Paul Young <paulyoungonline@gmail.com>
2016-09-15 20:11:38 -0700

    Build C library in pre-build step

--- bindings/haskell/Setup.hs
+++ bindings/haskell/Setup.hs
@@ -1,2 +1,21 @@
 import Distribution.Simple
-main = defaultMain
+    ( Args
+    , defaultMainWithHooks
+    , preBuild
+    , simpleUserHooks
+    )
+import Distribution.Simple.Setup
+    ( BuildFlags
+    , buildVerbosity
+    , fromFlag
+    )
+import Distribution.Simple.Utils (rawSystemExit)
+
+main = defaultMainWithHooks simpleUserHooks
+    { preBuild = \a b -> makeLib a b >> preBuild simpleUserHooks a b
+    }
+
+makeLib :: Args -> BuildFlags -> IO ()
+makeLib _ flags =
+    rawSystemExit (fromFlag $ buildVerbosity flags) "env"
+        ["make", "build", "--directory=../.."]
