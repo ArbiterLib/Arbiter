a46203d612638a1cd7f18ad914c19c2d9d432359
Dave Lee <davelee.com@gmail.com>
2016-09-17 20:22:06 -0700

    Start doing Arbiter::SemanticVersion

--- bindings/ruby/arbiter.c
+++ bindings/ruby/arbiter.c
@@ -1,6 +1,7 @@
 #include <ruby.h>
 #include <arbiter/Dependency.h>
 #include <arbiter/Types.h>
+#include <arbiter/Version.h>
 
 static bool value_equal(const void *first, const void *second) {
   VALUE a = (VALUE)first, b = (VALUE)second;
@@ -46,10 +47,57 @@ static VALUE project_identifier_initialize(VALUE self, VALUE value) {
   return self;
 }
 
+static VALUE semantic_version_allocate(VALUE klass) {
+  return Data_Wrap_Struct(klass, NULL, ArbiterFree, NULL);
+}
+
+static VALUE semantic_version_initialize(int argc, VALUE *argv, VALUE self) {
+  // SemanticVersion.new("1.2.3.pre.meta")
+  if (argc == 1) {
+    Check_Type(argv[0], T_STRING);
+    const char *string = StringValueCStr(argv[0]);
+
+    DATA_PTR(self) = ArbiterCreateSemanticVersionFromString(string);
+    return self;
+  }
+
+  if (argc < 3 || argc > 5) {
+    rb_error_arity(argc, 3, 5);
+  }
+
+  // SemanticVersion.new(1, 2, 3)
+  // SemanticVersion.new(1, 2, 3, "pre")
+  // SemanticVersion.new(1, 2, 3, "pre", "meta")
+  Check_Type(argv[0], T_FIXNUM);
+  Check_Type(argv[1], T_FIXNUM);
+  Check_Type(argv[2], T_FIXNUM);
+  unsigned major = FIX2UINT(argv[0]);
+  unsigned minor = FIX2UINT(argv[1]);
+  unsigned patch = FIX2UINT(argv[2]);
+
+  const char *prereleaseVersion = NULL;
+  if (argc >= 4) {
+    Check_Type(argv[3], T_STRING);
+    prereleaseVersion = StringValueCStr(argv[3]);
+  }
+  const char *buildMetadata = NULL;
+  if (argc >= 5) {
+    Check_Type(argv[4], T_STRING);
+    buildMetadata = StringValueCStr(argv[4]);
+  }
+
+  DATA_PTR(self) = ArbiterCreateSemanticVersion(major, minor, patch, prereleaseVersion, buildMetadata);
+  return self;
+}
+
 void Init_arbiter() {
   VALUE mArbiter = rb_define_module("Arbiter");
 
   VALUE cProjectIdentifier = rb_define_class_under(mArbiter, "ProjectIdentifier", rb_cObject);
   rb_define_alloc_func(cProjectIdentifier, project_identifier_allocate);
   rb_define_method(cProjectIdentifier, "initialize", project_identifier_initialize, 1);
+
+  VALUE cSemanticVersion = rb_define_class_under(mArbiter, "SemanticVersion", rb_cObject);
+  rb_define_alloc_func(cSemanticVersion, semantic_version_allocate);
+  rb_define_method(cSemanticVersion, "initialize", semantic_version_initialize, -1);
 }
