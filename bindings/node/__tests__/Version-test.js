b54783ebb7857376f9de78e8200386a476a11e0e
Paul Young <paulyoungonline@gmail.com>
2016-09-25 20:42:07 -0700

    Use JSVerify for testing

--- bindings/node/__tests__/Version-test.js
+++ bindings/node/__tests__/Version-test.js
@@ -1,29 +1,61 @@
 import { SemanticVersion } from "../";
+import jsc, { asciinestring, constant, forall, oneof, uint32 } from "jsverify";
 
-test("Version without prerelease version or build metadata", () => {
-  const major = 1;
-  const minor = 0;
-  const patch = 2;
-  const prereleaseVersion = null;
-  const buildMetadata = null;
-  const version = new SemanticVersion(major, minor, patch, prereleaseVersion, buildMetadata);
-  expect(version.getMajorVersion()).toBe(major);
-  expect(version.getMinorVersion()).toBe(minor);
-  expect(version.getPatchVersion()).toBe(patch);
-  expect(version.getPrereleaseVersion()).toBe(prereleaseVersion);
-  expect(version.getBuildMetadata()).toBe(buildMetadata);
-});
+const maybe = (type) => {
+  return oneof([
+    type,
+    constant(null),
+    constant(undefined)
+  ]);
+};
 
-test("Version with prerelease version and build metadata", () => {
-  const major = 1;
-  const minor = 2;
-  const patch = 3;
-  const prereleaseVersion = "alpha.0";
-  const buildMetadata = "dailybuild";
-  const version = new SemanticVersion(major, minor, patch, prereleaseVersion, buildMetadata);
-  expect(version.getMajorVersion()).toBe(major);
-  expect(version.getMinorVersion()).toBe(minor);
-  expect(version.getPatchVersion()).toBe(patch);
-  expect(version.getPrereleaseVersion()).toBe(prereleaseVersion);
-  expect(version.getBuildMetadata()).toBe(buildMetadata);
+// Test for arbitrary input values of `undefined`
+// but account for their return values being `null`.
+const isEquivalent = ({ arb, val }) => {
+  return arb === undefined ? val === null : val === arb;
+};
+
+// Allows a custom failure message when comparing boolean values.
+const expectToBe = (actual, expected, because) => expect(() => {
+  try {
+    expect(actual).toBe(expected);
+  } catch(error) {
+    throw new Error(because);
+  }
+}).not.toThrow();
+
+const checkGetters = (arbMajor, arbMinor, arbPatch, arbPrereleaseVersion, arbBuildMetadata) => {
+  const version = new SemanticVersion(arbMajor, arbMinor, arbPatch, arbPrereleaseVersion, arbBuildMetadata);
+
+  const major = version.getMajorVersion();
+  const minor = version.getMinorVersion();
+  const patch = version.getPatchVersion();
+  const prereleaseVersion = version.getPrereleaseVersion();
+  const buildMetadata = version.getBuildMetadata();
+
+  const prereleaseVersionIsEquivalent = isEquivalent({
+    arb: arbPrereleaseVersion,
+    val: prereleaseVersion
+  });
+
+  const buildMetadataIsEquivalent = isEquivalent({
+    arb: arbBuildMetadata,
+    val: buildMetadata
+  });
+
+  expect(major).toBe(arbMajor);
+  expect(minor).toBe(arbMinor);
+  expect(patch).toBe(arbPatch);
+  expectToBe(prereleaseVersionIsEquivalent, true, `${prereleaseVersion} !== ${arbPrereleaseVersion}`);
+  expectToBe(buildMetadataIsEquivalent, true, `${buildMetadata} !== ${arbBuildMetadata}`);
+
+  return major === arbMajor
+    && minor === arbMinor
+    && patch === arbPatch
+    && prereleaseVersionIsEquivalent
+    && buildMetadataIsEquivalent;
+};
+
+test("Version getters return correct values", () => {
+  jsc.assert(forall(uint32, uint32, uint32, maybe(asciinestring), maybe(asciinestring), checkGetters));
 });
