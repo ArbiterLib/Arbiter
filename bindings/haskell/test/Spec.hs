3a330c04a8c6d09ad436e240e8a36572dbc3dfe9
Paul Young <paulyoungonline@gmail.com>
2016-10-06 00:29:48 -0700

    Attempt to call exported functions in spec file

--- bindings/haskell/test/Spec.hs
+++ bindings/haskell/test/Spec.hs
@@ -1,2 +1,18 @@
+import Arbiter.Version
+  ( createSemanticVersion
+  , getMajorVersion
+  , getMinorVersion
+  , getPatchVersion
+  )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  let major = 1
+  let minor = 2
+  let patch = 3
+  let prereleaseVersion = "alpha.0"
+  let buildMetadata = "dailybuild"
+  let version = createSemanticVersion major minor patch prereleaseVersion buildMetadata
+  putStrLn $ show $ getMajorVersion version
+  putStrLn $ show $ getMinorVersion version
+  putStrLn $ show $ getPatchVersion version
