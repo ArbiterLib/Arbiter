e6806bf977b7e56479c37c7929c1a9f7f77798a7
Justin Spahr-Summers <justin@jspahrsummers.com>
2016-10-17 21:43:41 +0100

    Add mechanism for collecting resolution stats

---
+++ src/Stats.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifndef __cplusplus
+#error "This file must be compiled as C++."
+#endif
+
+#include <chrono>
+
+#include "Optional.h"
+
+namespace Arbiter {
+
+/**
+ * Used for benchmarking various aspects of dependency resolution.
+ */
+struct Stats final
+{
+  public:
+    using Clock = std::chrono::steady_clock;
+
+    Stats () = default;
+
+    Stats (Clock::time_point startTime)
+      : _startTime(std::move(startTime))
+    {}
+
+    unsigned _deadEnds{0};
+    unsigned _availableVersionFetches{0};
+    unsigned _dependencyListFetches{0};
+    size_t _cachedDependenciesSizeEstimate{0};
+    size_t _cachedAvailableVersionsSizeEstimate{0};
+    Optional<Clock::time_point> _startTime;
+    Optional<Clock::time_point> _endTime;
+};
+
+} // namespace Arbiter
